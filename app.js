9ea855e23b3884d4e93e9a89adada9d14e351e62
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-12-23 12:25:55 -0500

    begin form functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+const orderForm = document.getElementById('submit');
+
+orderForm.addEventListener('click', function(e) {
+	e.preventDefault();
+	const section = document.getElementById('order-section');
+	section.innerHTML =
+		'<section class="info-form-container container"><form action="#" id="info-form" class="info-form"><h4>Enter your details</h4><div class="info-input"><label for="info-name">Name:</label><input id="info-name"></div><div class="info-input"><label for="info-email">Email Address:</label><input id="info-email"></div><button type="submit" id="submit-story" class="btn btn-outline-secondary">Submit</button> <button id="close-submit" class="btn btn-outline-secondary">Close</button></form></section>';
+});
